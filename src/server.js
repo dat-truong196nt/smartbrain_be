06238e901eec84897c79cec90e5bf3c6e41b0c66
dat.truong <truongquocdat196nt@gmail.com>
2021-12-18 07:19:34 +0700

    port_config

--- src/server.js
+++ src/server.js
@@ -35,4 +35,4 @@ app.get('/profile/:id', profileHandler(database))
 app.put('/image', imageHandler(database))
 app.put('/imageUrl', apiCalling)
 
-app.listen(3000, () => console.log('Server is now running ...'));
+app.listen(process.env.PORT, () => console.log(`Server is now running at ${process.env.PORT}...`));
